7b8b8f5264e280f13d3072cc4d3200bb991888a9
VL250444 <VL250444@ED4-2-02>
2026-09-21 11:21:39 -0600

    Agregar archivos de proyecto.

---
+++ PawPort/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PawPort.Startup))]
+namespace PawPort
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
